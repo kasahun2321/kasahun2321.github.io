9fa3d35b727723c6675792c663d0c04792a99e9a
kasahun2321 <59380190+kasahun2321@users.noreply.github.com>
2020-09-01 20:29:31 -0500

    my first website half step

---
+++ kk.js
@@ -0,0 +1,36 @@
+// const words = ["Cat", "Ball", "Apple", "Dog"];
+// console.log(words);
+// const reversed_words = words.reverse();
+// console.log(reversed_words);
+// const phrase = words.join("");
+// console.log(phrase);
+// const sorted_words = words.sort();
+// console.log(sorted_words);
+
+
+// let x=["apple","ball","cool"]
+// let xg=[1,-2,3,-5]
+
+// console.log(xg.map(arr=>arr>0))
+// console.log(xg.filter(arr=>arr>0))
+
+// for(let i=0;i<x.length;i++)
+// {
+//     console.log(x[i].length);
+// }
+
+const arr1 = [1,2,3,4];
+for(let n of arr1){
+console.log(n*3);
+}
+
+console.log([1,2,3].reduce(n,sum) =>sum=sum+n)
+const myStr = "Hello world";
+for(let s of myStr){
+console.log(s);
+}
+
+const arr1 = [1,2,3,4];
+arr1.forEach(function(element){
+console.log(element);
+});
