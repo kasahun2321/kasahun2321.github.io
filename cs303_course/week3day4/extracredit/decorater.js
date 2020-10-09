d106a214d156101ded9a00d04d05df95b252c676
kasahun2321 <59380190+kasahun2321@users.noreply.github.com>
2020-10-09 01:15:28 -0500

    exam rework

---
+++ cs303_course/week3day4/extracredit/decorater.js
@@ -0,0 +1,71 @@
+/*eslint-disable*/
+{
+    function spy(func) {
+
+        function wrapper(...args) {
+          // using ...args instead of arguments to store "real" array in wrapper.calls
+          wrapper.calls.push(args);
+          return func.apply(this, args);
+        }
+      
+        wrapper.calls = [];
+      
+        return wrapper;
+      }
+      
+
+      describe("spy", function() {
+        it("records calls into its property", function() {
+          function work() {}
+      
+          work = spy(work);
+          assert.deepEqual(work.calls, []);
+      
+          work(1, 2);
+          assert.deepEqual(work.calls, [
+            [1, 2]
+          ]);
+      
+          work(3, 4);
+          assert.deepEqual(work.calls, [
+            [1, 2],
+            [3, 4]
+          ]);
+        });
+      
+       
+      
+      
+        
+      
+      });
+
+      
+}
+
+{
+
+
+    function delay(f, ms) {
+
+        return function(...args) {
+          let savedThis = this; // store this into an intermediate variable
+          setTimeout(function() {
+            f.apply(savedThis, args); // use it here
+          }, ms);
+        };
+      
+      }
+      
+}
+{
+
+    function debounce(func, ms) {
+        let timeout;
+        return function() {
+          clearTimeout(timeout);
+          timeout = setTimeout(() => func.apply(this, arguments), ms);
+        };
+      }
+    
+}
