35ac7a075e08b8b5044d07866719253c84e23746
kasahun2321 <59380190+kasahun2321@users.noreply.github.com>
2021-02-03 10:06:06 -0600

    correction

--- City_Library_Database_project/Js/fetchbooks.js
+++ City_Library_Database_project/Js/fetchbooks.js
@@ -14,12 +14,13 @@ fetch("https://elibraryrestapi.herokuapp.com/elibrary/api/book/list").then(funct
                 temp += "<tr>";
                 temp += "<td>" + counter++ + "</td>";
                 temp += "<td>" + kas.isbn + "</td>";
+                let identifierrow = kas.isbn;
                 temp += "<td>" + kas.title + "</td>";
                 temp += "<td>" + kas.overdueFee + "</td>";
                 temp += "<td>" + kas.publisher + "</td>";
                 temp += "<td>" + kas.datePublished + "</td>";
                 temp += "<td>" + ` <a onclick="edit()" class="btn btn-success">Edit</a>` + "</td>";
-                temp += "<td>" + `<button class="btn btn-danger" onclick="delete()">Delete</button>` + "</td></tr>";
+                temp += "<td>" + `<button class="btn btn-danger" onclick="deleter(${identifierrow})">Delete</button>` + "</td></tr>";
 
             })
 
@@ -29,5 +30,48 @@ fetch("https://elibraryrestapi.herokuapp.com/elibrary/api/book/list").then(funct
 
     });
 });
+
+function edit() { alert("edit is coming") }
+function deleter(x) {
+    console.log(x)
+
+    fetch("https://elibraryrestapi.herokuapp.com/elibrary/api/book/list")
+        .then(function (response) {
+            response.json().then(function (data) {
+
+                console.log(data);
+
+
+                data.forEach(kas => {
+
+                    if (x == kas.isbn) {
+                        let id = kas.bookId;
+                        console.log(id);
+
+                        const url = 'https://elibraryrestapi.herokuapp.com/elibrary/api/book/delete/${id}';
+
+                            // Awaiting fetch which contains  
+                            // method, headers and content-type 
+                            ( fetch(url, {
+                                method: 'DELETE',
+                                headers: {
+                                    'Content-type': 'application/json'
+                                }
+                            }))();
+
+                             
+
+
+                    }
+                })
+
+
+            });
+        });
+
+
+}
+
+
 
 
